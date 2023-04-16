16f318e15987ed4bda5d1dfa991528a5aa84afe0
ShadeManEXE <jflores99@rocketmail.com>
2023-04-15 21:26:25 -0700

    Enabled Message_Content so you can message the bot in a Server rather than a DM

--- src/main/java/org/bot/App.java
+++ src/main/java/org/bot/App.java
@@ -2,6 +2,7 @@ package org.bot;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.json.JSONArray;
 
 /**
@@ -12,7 +13,7 @@ public class App {
     static JSONArray assignments = new JSONArray();
 
     public static void main(String[] args) throws Exception {
-        JDA builder = JDABuilder.createDefault(API_keys.DiscordKey).build();
+        JDA builder = JDABuilder.createDefault(API_keys.DiscordKey).enableIntents(GatewayIntent.MESSAGE_CONTENT).build();
         builder.addEventListener(new MyListener());
     }
 }
