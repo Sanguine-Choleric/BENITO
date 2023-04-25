adc8a5f9ce2a552a88d1683706ded0b437cf2de0
mrb1ue <3781565-mrb1u@users.noreply.gitlab.com>
2023-04-25 15:47:37 -0700

    BENITO-7 Reverting temp message builder. That's a problem for u UI/UX guys

--- src/main/java/org/bot/MyListener.java
+++ src/main/java/org/bot/MyListener.java
@@ -34,10 +34,13 @@ public class MyListener extends ListenerAdapter {
                         throw new RuntimeException(e);
                     }
                 }
-                channel.sendMessage(messageBuilder(App.db.getCourses_AL(), "name")).queue();
+
+                // channel.sendMessage(messageBuilder(App.db.getCourses_AL(), "name")).queue();
+                for (int i = 0; i < App.db.getCourses_AL().size(); i++) {
+                    channel.sendMessage(App.db.getCourses_AL().get(i).getCourseName()).queue();
+                }
             }
             // Now grabs all assignments
-            // TODO: BUG - 2000 char limit per message. This command regularily goes over limit
             case "!hw" -> {
                 MessageChannel channel = event.getChannel();
 
@@ -57,7 +60,9 @@ public class MyListener extends ListenerAdapter {
                     throw new RuntimeException(e);
                 }
 
-                channel.sendMessage(messageBuilder(App.db.getAllAss_AL(), "name")).queue();
+                for (int i = 0; i < App.db.getAllAss_AL().size(); i++) {
+                    channel.sendMessage(App.db.getAllAss_AL().get(i).getAssName()).queue();
+                }
             }
 
             case "!upcoming" -> {
@@ -88,39 +93,11 @@ public class MyListener extends ListenerAdapter {
                     throw new RuntimeException(e);
                 }
 
-                channel.sendMessage(messageBuilder(App.db.getUpcomingAss_AL(), "name")).queue();
-            }
-        }
-    }
-
-    // TEMP messagbuilder for demo only
-    // TODO: UI/UX guys rewrite this; needs to solve 2000char limit on messages
-    protected static String messageBuilder(ArrayList<?> source, String key) {
-        String message = "";
-
-        // Java hackery to take Assignment and Course Objects
-        for (int i = 0; i < source.size(); i++) {
-            Object object = source.get(i);
-
-            if (key == "name") {
-                if (object instanceof Course) {
-                    Course course = (Course) object;
-                    message = message.concat(course.getCourseName() + "\n");
-                } else if (object instanceof Assignment) {
-                    Assignment assignment = (Assignment) object;
-                    message = message.concat(assignment.getAssName() + "\n");
-                }
-            } else if (key == "id") {
-                if (object instanceof Course) {
-                    Course course = (Course) object;
-                    message = message.concat(course.getCourseID() + "\n");
-                } else if (object instanceof Assignment) {
-                    Assignment assignment = (Assignment) object;
-                    message = message.concat(assignment.getAssID() + "\n");
+                // channel.sendMessage(messageBuilder(App.db.getUpcomingAss_AL(), "name")).queue();
+                for (int i = 0; i < App.db.getUpcomingAss_AL().size(); i++) {
+                    channel.sendMessage(App.db.getUpcomingAss_AL().get(i).getAssName()).queue();
                 }
             }
-            
         }
-        return message;
     }
 }
--- src/test/java/org/bot/MyListenerTest.java
+++ src/test/java/org/bot/MyListenerTest.java
@@ -1,40 +1,5 @@
 package org.bot;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.ArrayList;
-
-import org.json.JSONObject;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 public class MyListenerTest {
-    JSONObject jsonCourse = new JSONObject();
-    Course course;
-    ArrayList<Course> courses = new ArrayList<Course>();
-    ArrayList<Assignment> assignments = new ArrayList<Assignment>();
 
-    @Test
-    void testMessageBuilderCourse() {
-        jsonCourse.put("id", 100);
-        jsonCourse.put("name", "Course 1");
-        jsonCourse.put("description", "This is a course, but this description shouldn't show up in course list.");
-        course = new Course(jsonCourse);
-        courses.add(course);
-        String message = MyListener.messageBuilder(courses, "name");
-        assertEquals("Course 1\n", message, "Testing message builder");
-    }
-
-    @Test
-    void testMessageBuilderAssignment() {
-        JSONObject jsonAssignment = new JSONObject();
-        jsonAssignment.put("id", 100);
-        jsonAssignment.put("name", "Assignment 1");
-        jsonAssignment.put("due_at", "2021-04-20T23:59:00Z");
-        jsonAssignment.put("course_id", 100);
-        Assignment assignment = new Assignment(jsonAssignment);
-        assignments.add(assignment);
-        String message = MyListener.messageBuilder(assignments, "name");
-        assertEquals("Assignment 1\n", message, "Testing message builder");
-    }
 }
