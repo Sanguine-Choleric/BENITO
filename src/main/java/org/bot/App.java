acefc746c7afa07f000bacb5c9b0096bcc741612
Jeff H <130211779+Sanguine-Choleric@users.noreply.github.com>
2023-05-11 17:59:53 -0700

    Better initial bot loading * DB populates before bot launches now

--- src/main/java/org/bot/App.java
+++ src/main/java/org/bot/App.java
@@ -8,15 +8,14 @@ public class App {
     static Database db = new Database();
 
     public static void main(String[] args) throws Exception {
-        JDA builder = JDABuilder.createDefault(API_keys.DiscordKey).enableIntents(GatewayIntent.MESSAGE_CONTENT).build();
-//        JDA builder = JDABuilder.createDefault(API_keys.DiscordKey).build();
-        builder.addEventListener(new MyListener());
-
         // Pre-populating database
         System.out.println("Populating Courses");
         db.courseLOAD(CanvasGet.getCourses());
         System.out.println("Populating Assignments");
         db.assLOAD(CanvasGet.getAllAssignments());
         System.out.println("Bot is ready");
+
+        JDA builder = JDABuilder.createDefault(API_keys.DiscordKey).enableIntents(GatewayIntent.MESSAGE_CONTENT).build();
+        builder.addEventListener(new MyListener());
     }
 }
