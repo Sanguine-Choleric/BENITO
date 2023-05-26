a1429ca01cc2a1c0575738a850622de2b8131510
Jeff H <130211779+Sanguine-Choleric@users.noreply.github.com>
2023-05-25 22:42:38 -0700

    MessageBuilder unit tests

--- src/main/java/org/bot/messageBuilder.java
+++ src/main/java/org/bot/messageBuilder.java
@@ -8,28 +8,52 @@ import java.util.ArrayList;
 
 public class messageBuilder {
 
+
+    /**
+     * Converts an arraylist of assignments into pretty strings. Works with Discord's 2000-character limit.
+     *
+     * @param assignments ArrayList of assignments to convert to a string
+     * @return ArrayList of strings, each of which is a message to be sent to Discord
+     */
     public static ArrayList<String> convert(ArrayList<Assignment> assignments) {
         ArrayList<String> strings = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
         int charCount = 0;
+
+        // I am the way into the city of woe
         for (Assignment assignment : assignments) {
+
+            // I am the way into eternal pain
             String assignmentString = assignmentToString(assignment);
+
+            // I am the way to go among the lost
             if (charCount + assignmentString.length() + 1 > 2000) {
-                // start a new string
+
+                // Justice caused my high architect to move
                 strings.add(sb.toString());
                 sb = new StringBuilder();
                 charCount = 0;
             }
+
+            // Divine omnipotence created me
             if (charCount > 0) {
+
+                // The highest wisdom, and the primal love
                 sb.append("\n");
                 charCount += 1;
             }
+
+            // Before me there were no created things
             sb.append(assignmentString);
             charCount += assignmentString.length();
         }
+
+        // But those that last forever - as do I
         if (sb.length() > 0) {
             strings.add(sb.toString());
         }
+
+        // Abandon all hope, ye who enter here
         return strings;
     }
 
@@ -41,6 +65,9 @@ public class messageBuilder {
     }
 
     private static String formatDueDate(LocalDateTime dueDate) {
+        if (dueDate == null) {
+            return "00/00 00:00";
+        }
         return dueDate.format(DateTimeFormatter.ofPattern("MM/dd HH:mm"));
     }
 
@@ -49,14 +76,15 @@ public class messageBuilder {
         for (int i = 0; i < App.db.getCourses_AL().size(); i++) {
             if (App.db.getCourses_AL().get(i).getCourseID() == courseId) {
                 courseName = App.db.getCourses_AL().get(i).getCourseName();
-                return courseName.split("\\s+")[0];
+                courseName = courseName.split("\\s+")[0];
+                return courseName.length() > 7 ? courseName.substring(0, 7) : String.format("%-7s", courseName);
             }
         }
-        return "";
+        return "       ";
     }
 
     private static String formatAssignmentName(String AssignmentName) {
-        int MAX_LENGTH = 50;
+        int MAX_LENGTH = 75;
         String stripped = AssignmentName.replaceAll("[\\\\*_`\\[\\]]", "\\\\$0");
         if (stripped.length() > MAX_LENGTH) {
             return stripped.substring(0, MAX_LENGTH) + "...";
--- src/test/java/org/bot/messageBuilderTest.java
+++ src/test/java/org/bot/messageBuilderTest.java
@@ -5,7 +5,10 @@ import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MessageBuilderTest {
     Database db;
@@ -23,8 +26,10 @@ class MessageBuilderTest {
     void convert() {
         App.db = new Database();
 
+        // Regular list of courses
         jsonCourses = new JSONArray();
 
+        // Creating single course
         jsonCourse = new JSONObject();
         jsonCourse.put("id", 100);
         jsonCourse.put("name", "CSC101 Course 1");
@@ -33,6 +38,7 @@ class MessageBuilderTest {
 
         App.db.courseLOAD(jsonCourses);
 
+        // Regular assignment
         jsonAssignments = new JSONArray();
         jsonAssignment = new JSONObject();
         jsonAssignment.put("id", 1000);
@@ -42,17 +48,76 @@ class MessageBuilderTest {
         jsonAssignment.put("has_submitted_submissions", true);
         jsonAssignments.put(jsonAssignment);
 
+        // Assignment with null date
         jsonAssignment = new JSONObject();
         jsonAssignment.put("id", 1010);
         jsonAssignment.put("name", "Assignment 2");
-        jsonAssignment.put("due_at", "2023-04-24T06:59:59Z");
+        jsonAssignment.put("due_at", JSONObject.NULL);
         jsonAssignment.put("course_id", 100);
+        jsonAssignment.put("has_submitted_submissions", true);
+        jsonAssignments.put(jsonAssignment);
+
+        // Assignment with long name
+        jsonAssignment = new JSONObject();
+        jsonAssignment.put("id", 1020);
+        jsonAssignment.put("name", "Assignment 3 with a very long name that should be truncated and have three periods appended to the end");
+        jsonAssignment.put("due_at", JSONObject.NULL);
+        jsonAssignment.put("course_id", 100);
+        jsonAssignment.put("has_submitted_submissions", true);
+        jsonAssignments.put(jsonAssignment);
+
+        // Assignment with no matching course
+        jsonAssignment = new JSONObject();
+        jsonAssignment.put("id", 1030);
+        jsonAssignment.put("name", "Assignment 4");
+        jsonAssignment.put("due_at", JSONObject.NULL);
+        jsonAssignment.put("course_id", 101);
         jsonAssignment.put("has_submitted_submissions", true);
         jsonAssignments.put(jsonAssignment);
 
         App.db.assLOAD(jsonAssignments);
 
         String s = messageBuilder.convert(App.db.getAllAss_AL()).get(0);
-        assertEquals("`04/22 23:59 | CSC101 |` Assignment 1\n" + "`04/23 23:59 | CSC101 |` Assignment 2", s);
+        assertEquals("""
+                `04/22 23:59 | CSC101  |` Assignment 1
+                `00/00 00:00 | CSC101  |` Assignment 2
+                `00/00 00:00 | CSC101  |` Assignment 3 with a very long name that should be truncated and have three ...
+                `00/00 00:00 |         |` Assignment 4""", s);
+    }
+
+    @Test
+    void convert_large() {
+        // Initializing blank database with single course
+        App.db = new Database();
+        jsonCourses = new JSONArray();
+        jsonCourse = new JSONObject();
+        jsonCourse.put("id", 100);
+        jsonCourse.put("name", "CSC101");
+        jsonCourses.put(jsonCourse);
+        App.db.courseLOAD(jsonCourses);
+
+        // Initializing list of assignments
+        jsonAssignments = new JSONArray();
+
+        // Creating 100 assignments to exceed 2000char limit
+        for (int i = 0; i < 100; i++) {
+            jsonAssignment = new JSONObject();
+            jsonAssignment.put("id", 1000 + i);
+            jsonAssignment.put("name", "Assignment " + i);
+            jsonAssignment.put("due_at", JSONObject.NULL);
+            jsonAssignment.put("course_id", 100);
+            jsonAssignment.put("has_submitted_submissions", true);
+            jsonAssignments.put(jsonAssignment);
+        }
+
+        App.db.assLOAD(jsonAssignments);
+
+        ArrayList<String> sArr = messageBuilder.convert(App.db.getAllAss_AL());
+        assertTrue(sArr.size() > 1);
+
+        for (String s : sArr) {
+            assertTrue(s.length() < 2000);
+        }
+
     }
 }
